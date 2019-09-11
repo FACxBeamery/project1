437e4a93ca206bee669dc7799f35a492c3b4c904
Martha Lambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-09-11 16:26:50 +0100

    input validation > Relates #3

--- script.js
+++ script.js
@@ -6,12 +6,41 @@ function calculateAge(form){
     var month = form.month.value;
     var year = form.year.value;
 
+
     var dob = new Date(year, month -1, day); 
     var dateNow = new Date();
-    var ageInDays = Math.floor((dateNow.getTime() - dob.getTime())/oneDay);
 
-    document.getElementById("output").innerHTML = ageInDays;
-    return '0'
+    if ((dob.getFullYear() != year) || (dob.getMonth() != month - 1) || (dob.getDate() != day)){
+        document.getElementById("output").innerHTML = 'Please enter a valid date!';
+        document.getElementById("output").style.color = '#F00';
+        return 'Invalid date'
+    }
+
+    var dateDiff = new Date(dateNow.getTime() - dob.getTime());
+
+    var ageInYears = dateDiff.getFullYear() - 1970;
+    var ageInMonths = dateDiff.getMonth();
+    var ageInDays = dateDiff.getDate() - 1;
+    console.log(dateDiff);
+    console.log(ageInYears);
+    console.log(ageInMonths);
+    console.log(ageInDays);
+
+    if (ageInDays < 0){
+        document.getElementById("output").innerHTML = 'I don\'t believe you!';
+        return 'Invalid date'
+    }
+
+    var multDays = (ageInDays != 1)?('s'):('')
+    console.log(multDays)
+    var multMonths =  (ageInMonths != 1)?('s'):('')
+    var multYears = (ageInYears != 1)?('s'):('')
+
+    var age = 'You are ' + ageInYears + ' year'+  multYears + ' ' + ageInMonths + ' month' + multMonths + ' and ' + ageInDays + ' day' + multDays + ' old.' ;
+    
+    document.getElementById("output").innerHTML = age;
+    console.log(age);
+    return 'Age updated'
     
 
 }
