aa2347466bb98b172e519c612a4763f6cb228209
Martha Lambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-09-12 10:16:01 +0100

    images added

--- script.js
+++ script.js
@@ -6,22 +6,28 @@ function calculateAge(form){
     var dob = new Date(year, month -1, day); 
     var dateNow = new Date();
 
+    
+   
     if ((dob.getFullYear() != year) || (dob.getMonth() != month - 1) || (dob.getDate() != day)){
-        document.getElementById("output").innerHTML = 'Please enter a valid date!';
-        document.getElementById("output").style.color = '#F00';
-        return 'Invalid date'
+        messageWriter('Please enter a valid date!', '#F00');
+        return
+    }
+
+    if (dob.getFullYear() % 4 != 0){
+        ageInDays--
     }
 
     var dateDiff = new Date(dateNow.getTime() - dob.getTime());
 
     var ageInYears = dateDiff.getFullYear() - 1970;
     var ageInMonths = dateDiff.getMonth();
-    var ageInDays = dateDiff.getDate() - 1;
+    var ageInDays = dateDiff.getDate();
 
-    if (ageInDays < 0){
-        var outputText = document.createTextNode('I don\'t believe you!');
-        outputText.appendChild(output);
-        return 'Invalid date'
+
+
+    if (dateDiff < 0){
+        messageWriter('I don\'t believe you!', '#F00');
+        return 
     }
 
     var multDays = (ageInDays != 1)?('s'):('')
@@ -30,14 +36,32 @@ function calculateAge(form){
 
     var age = 'You are ' + ageInYears + ' year'+  multYears + ' ' + ageInMonths + ' month' + multMonths + ' and ' + ageInDays + ' day' + multDays + ' old.' ;
     
+    messageWriter(age, '#FFF');
+
+    var image = document.getElementById("cartoon")
+    if ((dob.getMonth()==dateNow.getMonth()) && (dob.getDate()==dateNow.getDate())){
+        image.src = "img/birthday.png";
+    }
+
+    else {
+        image.src = "img/question.png";
+    }
+    return
+
+}
+
+function messageWriter(str, col){
+    var image = document.getElementById("cartoon")
     var output = document.getElementById("output");
     if (output.childNodes.length > 0 ){
         output.removeChild(output.childNodes[0]);
     }
-    
-    var outputText = document.createTextNode(age);
+    var outputText = document.createTextNode(str);
     output.appendChild(outputText);
-    return 'Age updated'
-    
+    output.style.color = col
 
+    if (col == '#F00'){
+        image.src = "img/confused.png";
+    }
+    return
 }
